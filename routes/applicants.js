078836edb6eac06a3b980170638aa84ab3f1bdee
SteelBurrito <antouniokevin@gmail.com>
2018-09-14 12:40:22 +1000

    enable CORS

--- routes/applicants.js
+++ routes/applicants.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var cors = require('cors');
 var applicant_controller = require('../controllers/applicant.controller.js');
 
+router.all('*', cors());
 router.post('/', applicant_controller.create);
 router.get('/', applicant_controller.findall);
 router.get('/applicantID', applicant_controller.findone);
--- routes/tests.js
+++ routes/tests.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var cors = require('cors');
 var test_controller = require('../controllers/test.controller.js');
 
+router.all('*', cors());
 router.post('/', test_controller.create);
 router.get('/', test_controller.findall);
 router.get('/testID', test_controller.findone);
