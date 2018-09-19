813c63ae1ca96f00d7e8092250f383d499f97185
SteelBurrito <antouniokevin@gmail.com>
2018-09-19 21:53:23 +1000

    fixed test route parameter

--- routes/tests.js
+++ routes/tests.js
@@ -2,12 +2,14 @@ var express = require('express');
 var router = express.Router();
 var cors = require('cors');
 var test_controller = require('../controllers/test.controller.js');
+var authentication_controller = require('../controllers/authentication.controller.js');
 
 router.all('*', cors());
 router.post('/', test_controller.create);
 router.get('/', test_controller.findall);
-router.get('/testID', test_controller.findone);
-router.put('/testID', test_controller.update);
-router.delete('/testID', test_controller.delete);
+router.get('/:testID', test_controller.findone);
+router.get('/applicant-aptitude-test/:testID', authentication_controller.tokenrequired, test_controller.findone);
+router.put('/:testID', test_controller.update);
+router.delete('/:testID', test_controller.delete);
 
 module.exports = router;
